d952ea9feb6dc9eff92cf41d6871d2b662236280
Sousa <jonatas.f.sousa@accenture.com>
2018-11-11 08:12:20 -0200

    First Commit

---
+++ client/app/mySession/mySession.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup,  FormBuilder,  Validators , FormControl} from '@angular/forms';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-mySession',
+  templateUrl: 'mySession.component.html',
+  styleUrls: ['mySession.component.css']
+})
+export class MySessionComponent implements OnInit {
+  
+  // registerForm: FormGroup;
+  // func: FormControl;
+  // login: FormControl;
+  // submitted = false;
+
+
+  
+  ngOnInit() {
+
+    // this.func = new FormControl("", Validators.required);
+    // this.login = new FormControl("", Validators.required);
+    // this.registerForm = new FormGroup({
+    //   func: this.func,
+    //   login: this.login,
+    // })
+}
+
+onSubmit(fxn:any){
+    // console.log("Form Submitted!");
+    // console.log(this.registerForm.value);
+    // console.log(fxn.value)
+    // console.log(fxn.value)
+    // try{
+    //   return fxn(this.registerForm.value.login);
+    // }catch(err) {
+    // 		alert(err)
+    // 		return false;
+    // 	}
+  }
+// }
+
+errWrap(func:any, form:any){
+	try {
+    console.log(func)
+    console.log(form);
+    return func(form);
+	} catch(err) {
+		alert("WebRTC is currently only supported by Chrome, Opera, and Firefox");
+		return false;
+	}
+}
+}
+ 
